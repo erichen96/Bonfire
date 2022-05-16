1fa8677e8957859f4d28d34978a58c70a55c6112
Eric Chen <ejc196@gmail.com>
2022-05-15 17:28:41 -0700

    Added initial map file, for structure purposes

---
+++ Bonfire/Controllers/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  Bonfire
+//
+//  Created by Eric Chen on 5/14/22.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet private var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
